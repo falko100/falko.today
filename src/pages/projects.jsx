265422f335af42c57ab3b0aedfbac41a9bd0a833
Falko Woudstra <falko@coddin.nl>
2022-09-14 13:24:45 +0200

    Update projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,38 +7,9 @@ import logoCoddin from '@/images/logos/coddin.jpeg'
 
 const projects = [
   {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoCoddin,
-  },
-  {
-    name: 'Animaginary',
-    description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCoddin,
-  },
-  {
-    name: 'HelioStream',
-    description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCoddin,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCoddin,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
+    name: 'Coddin',
+    description: 'New website for Coddin',
+    link: { href: 'https://coddin.nl', label: 'coddin.nl' },
     logo: logoCoddin,
   },
 ]
@@ -78,7 +49,7 @@ export default function Projects() {
                 <Image
                   src={project.logo}
                   alt=""
-                  className="h-8 w-8"
+                  className="h-full w-full"
                   unoptimized
                 />
               </div>
