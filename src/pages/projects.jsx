8479a977f88e7f9e221a7250f2a29e707c1e1a7b
Falko Woudstra <falko@coddin.nl>
2022-09-21 23:31:48 +0200

    Add projects and new blogpost

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,14 +3,39 @@ import Head from 'next/head'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoCoddin from '@/images/logos/coddin.jpeg'
+import coddinSite from '@/images/projects/coddin-site.png'
+import elektramat from '@/images/projects/elektramat.png'
+import tommie from '@/images/projects/tommie.png'
 
 const projects = [
   {
     name: 'Coddin',
-    description: 'New website for Coddin',
-    link: { href: 'https://coddin.nl', label: 'coddin.nl' },
-    logo: logoCoddin,
+    description:
+      'A new website for Coddin. Built with PayloadCMS, Nuxt and a lot of custom CSS.',
+    link: { href: 'https://coddin.nl', label: 'coddin.nl', target: '_blank' },
+    logo: coddinSite,
+  },
+  {
+    name: 'Elektramat Generatoren',
+    description:
+      'Custom product generators for fuseboxes, electrical components and charging stations',
+    link: {
+      href: 'https://elektramat.nl',
+      label: 'elektramat.nl',
+      target: '_blank',
+    },
+    logo: elektramat,
+  },
+  {
+    name: 'TommieTheCat Box builder',
+    description:
+      'Custom box build for catfood subscriptions. Completely headless and build with TypeScript and Vue3.',
+    link: {
+      href: 'https://boxbuilder.tommiethecat.com',
+      label: 'boxbuilder.tommiethecat.com',
+      target: '_blank',
+    },
+    logo: tommie,
   },
 ]
 
@@ -32,12 +57,12 @@ export default function Projects() {
         <title>Projects - Falko Woudstra</title>
         <meta
           name="description"
-          content="Things I’ve made trying to put my dent in the universe."
+          content="These are some of the projects I am proud of."
         />
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        title="These are some of the projects I am proud of."
+        intro="I've worked on more projects than I can remember. These are some of the projects I am proud of. When I'm not working on projects for clients, I'm working on my own projects. This blog is one of them. The main reason I started this blog was to learn React and Next.js. I also use this blog to write about things I've learned, and hope others can learn from it as well."
       >
         <ul
           role="list"
@@ -45,16 +70,21 @@ export default function Projects() {
         >
           {projects.map((project) => (
             <Card as="li" key={project.name}>
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <div className="relative z-10 flex h-40 w-full items-center justify-center bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                 <Image
                   src={project.logo}
                   alt=""
-                  className="h-full w-full rounded-full"
+                  className="h-full w-full object-cover object-top"
                   unoptimized
                 />
               </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={project.link.href}>{project.name}</Card.Link>
+                <Card.Link
+                  href={project.link.href}
+                  target={project.link.target}
+                >
+                  {project.name}
+                </Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
