f3f61e4d1a9ac029aaa604739f28731cf4320762
Falko Woudstra <falko@coddin.nl>
2022-09-21 23:52:23 +0200

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://falko.today',
+  generateRobotsTxt: true,
+  exclude: [],
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+
+    ],
+  },
+}
