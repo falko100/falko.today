7e8c0b7a7e5ab4ee676086b7c418fe920757befb
Falko Woudstra <falko@coddin.nl>
2022-09-24 11:53:15 +0200

    Return revalidate as part of the static props instead of the props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -308,9 +308,9 @@ export default function Home({
             </p>
             <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
               Fun fact; I am using Incremental Static Regeneration to generate
-              this website. Its a new feature in Next.js 10.2.0. Its a hybrid of
-              static and server-side rendering. Its a great way to get the best
-              of both worlds. The last time the page was regenerated was on{' '}
+              this website. Its a somewhat new feature in Next.js. Its a hybrid
+              of static and server-side rendering. Its a great way to get the
+              best of both worlds. The last time the page was regenerated was on{' '}
               {generatedTime}. It should revalidate every minute.
             </p>
             <div className="mt-6 flex gap-6">
@@ -363,8 +363,8 @@ export async function getStaticProps() {
 
   const date = new Date();
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       generatedTime:
         date.toLocaleDateString('nl-NL') +
         ' at ' +
