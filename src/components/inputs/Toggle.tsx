954844889ef9ee0a25b9f83415be2d8ce10ede35
Falko Woudstra <falko@coddin.nl>
2022-09-24 22:13:58 +0200

    Actual Toggle darkmode, not just the label and text

--- src/components/inputs/Toggle.tsx
+++ src/components/inputs/Toggle.tsx
@@ -27,15 +27,15 @@ export default function Toggle({
         checked={enabled}
         onChange={setEnabled}
         className={classNames(
-          enabled ? 'bg-teal-600' : 'bg-gray-200',
-          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'
+          enabled ? 'bg-teal-600' : 'bg-gray-200 dark:bg-gray-700',
+          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-offset-2 dark:ring-offset-black'
         )}
       >
         <span
           aria-hidden="true"
           className={classNames(
             enabled ? 'translate-x-5' : 'translate-x-0',
-            'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+            'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out dark:bg-zinc-200'
           )}
         />
       </Switch>
